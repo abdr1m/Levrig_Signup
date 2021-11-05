153d03c2120db39d55e2bf1abd8c3765561334fc
abdr1m <63007061+abdr1m@users.noreply.github.com>
2021-11-05 17:28:16 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  server: {
+    port: 1960 // default: 3000
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - levering',
